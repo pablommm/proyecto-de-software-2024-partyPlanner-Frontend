005a358c6a2c468c6d7070f0e6cba4041acb171a
marycairo <marycairo2@gmail.com>
2024-05-02 23:40:36 -0300

    actualizo

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { Box, ThemeProvider, createTheme } from '@mui/material'
 import './App.css'
 import { PartyRouter } from './router'
 import UserContext from 'src/Services/context'
-import UsuarioService from '../src/Services/login.service'
 import { useState } from 'react'
 
 function App() {
-  
+
   const theme = createTheme({
     palette: {
       primary: {
-        main: '#171718', 
+        main: '#171718',
       },
       secondary: {
-        main: '#ff9e80', 
+        main: '#ff9e80',
       },
     },
   })
@@ -23,20 +22,20 @@ function App() {
   return (
     //<UserContext.Provider value={{name: "nada"}}>
     <UserContext.Provider value={[user, setUser]}>
-  <ThemeProvider theme={theme}>
-      <Box
-         sx={{
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
             display: 'flex',
             flexDirection: 'column',
             minHeight: '100vh',
             minWidth: '100vw'
-            }}
-          >
-      <PartyRouter />
-    </Box>
-</ThemeProvider>
-</UserContext.Provider>
-      )
+          }}
+        >
+          <PartyRouter />
+        </Box>
+      </ThemeProvider>
+    </UserContext.Provider>
+  )
 }
 
 export default App
