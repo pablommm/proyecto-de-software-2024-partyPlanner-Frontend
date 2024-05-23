14a920ede5d8184502bc6334f8533fcb23ae087a
pablommm <murgiapablo@gmail.com>
2024-05-23 20:39:31 -0300

    se agregan detalles a barra de busqueda

--- src/views/principalView.jsx
+++ src/views/principalView.jsx
@@ -36,6 +36,13 @@ const PrincipalView = () => {
         setTerminoDeBúsqueda(evento.target.value.toLowerCase()) // Normalizar término de búsqueda
       }
 
+      const manejarPresionarEnter = (event) => {
+        if (event.key === 'Enter') {
+          // Implement your search logic here
+          console.log('Realizar búsqueda con:', términoDeBúsqueda)
+        }
+      }
+
     return (
         <Container className="main" style={{ marginBottom: "10rem" }}>
             <TextField
@@ -45,9 +52,11 @@ const PrincipalView = () => {
           fullWidth
           value={términoDeBúsqueda}
           onChange={manejarCambioBúsqueda}
+          onKeyDown={manejarPresionarEnter} // Agregar el manejador de Enter
           placeholder="Nombre del salón o localidad"
           helperText="Filtrar por nombre del salón o localidad" // Texto de ayuda opcional
         />
+        
             <Grid container spacing={3} justifyContent="center">
                 {instalaciones.map((instalacion, index) =>
                     <Grid item key={index}>
