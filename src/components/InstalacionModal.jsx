f95bb5b42a0b55657e506e5883dfe1f434a1ca7d
Juanignacioz <jzotelo@estudiantes.unsam.edu.ar>
2024-05-29 23:16:09 -0400

    sE

--- src/components/InstalacionModal.jsx
+++ src/components/InstalacionModal.jsx
@@ -1,54 +1,115 @@
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import PropTypes from 'prop-types'
+<<<<<<< HEAD
 import { TextField, Button, Typography, Snackbar, SnackbarContent } from "@mui/material"
+=======
+import { TextField, Button, Typography } from '@mui/material'
+>>>>>>> 9841c9d ( se agrega funcionalidad al formulario de nuevas instalaciones cargar imagenes al momento de generar un nuevo registro)
 import { useState, useEffect } from 'react'
 import instalacionService from 'src/Services/instalaciones.service'
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 300,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 300,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
 }
 
-const InstalacionModal = ({ openModal, cerrarModal, instalacion, actualizarInstalacion }) => {
-    const [nombre, setNombre] = useState('')
-    const [localidad, setLocalidad] = useState('')
-    const [costoInstalacion, setCostoInstalacion] = useState('')
-    const [capacidadInstalacion, setCapacidadInstalacion] = useState('')
-    const [descripcionInstalacion, setDescripcionInstalacion] = useState('')
-    const [imagenPrincipal, setImagenPrincipal] = useState('')
-    const [image, setImage] = useState([])
+const InstalacionModal = ({
+  openModal,
+  cerrarModal,
+  instalacion,
+  actualizarInstalacion,
+}) => {
+  const [nombre, setNombre] = useState('')
+  const [localidad, setLocalidad] = useState('')
+  const [costoInstalacion, setCostoInstalacion] = useState('')
+  const [capacidadInstalacion, setCapacidadInstalacion] = useState('')
+  const [descripcionInstalacion, setDescripcionInstalacion] = useState('')
+  const [imagenPrincipal, setImagenPrincipal] = useState('')
+  const [image, setImage] = useState([])
+    
 
-    const uploadToServer = async (e) => {
-        const imageFile = e.target.files[0]
-        const url = `https://api.imgbb.com/1/upload?&key=aa6bb07c1670d6b0bca14de0df39782f&name=${imageFile.name})`
-        const data = new FormData
-        data.append ("image",imageFile)
-        try {
-        const response = await fetch(url, {	
-        method: "POST",
+  const uploadToServer = async (e) => {
+    const imageFile = e.target.files[0]
+    const url = `https://api.imgbb.com/1/upload?&key=bf6dc07a9169a884477289872654d8fd&name=${imageFile.name})`
+    const data = new FormData()
+    data.append('image', imageFile)
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
         body: data,
-        })
-        
-        const responseData = await response.json()
-        console.log('que tengo aca :' + responseData)
+      })
+      console.log('que tengo aca -1 :' + url)
+      console.log('que tengo aca -1 :' + data)
+      const responseData = await response.json()
+      console.log('que tengo aca :' + responseData)
 
-        console.log('que tengo aca 2 :' + responseData.data.url)
-        
-        setImage(responseData.data.url)
-        } catch (error) {
-        console.error (error)
+      console.log('que tengo aca 2 :' + responseData.data.url)
+      console.log('que tengo aca 3 :' + (responseData.data.url).toString())
+      setImage(responseData.data.url)
+    } catch (error) {
+      console.error(error)
     }
-        }
-        
+  }
 
+  useEffect(() => {
+    if (instalacion) {
+      setNombre(instalacion.nombreDeInstalacion || '')
+      setLocalidad(instalacion.localidadDeInstalacion || '')
+      setCostoInstalacion(instalacion.costoDeInstalacion || '')
+      setCapacidadInstalacion(instalacion.capacidadInstalacion || '')
+      setDescripcionInstalacion(instalacion.descripcionDeInstalacion || '')
+      setImagenPrincipal(instalacion.imagenPrincipal || '')
+    } else {
+      limpiarDatos()
+    }
+  }, [instalacion])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const datosInstalacion = {
+      nombreDeInstalacion: nombre,
+      localidadDeInstalacion: localidad,
+      costoDeInstalacion: costoInstalacion,
+      capacidadInstalacion: capacidadInstalacion,
+      descripcionDeInstalacion: descripcionInstalacion,
+      imagenPrincipal: imagenPrincipal,
+    }
+
+    try {
+      if (instalacion) {
+        const respuestaEditar = await instalacionService.actualizarInstalacion({
+          ...datosInstalacion,
+          id: instalacion.id,
+        })
+        console.log('Respuesta de edición de instalación:', respuestaEditar)
+        console.log('Instalación editada exitosamente.')
+      } else {
+        const respuestaCrearInstalacion =
+          await instalacionService.crearInstalacion(datosInstalacion)
+        console.log(
+          'Respuesta de creación de instalación:',
+          respuestaCrearInstalacion,
+        )
+        console.log('Instalación creada exitosamente.')
+      }
+      cerrarModal()
+      limpiarDatos()
+      actualizarInstalacion()
+    } catch (error) {
+      console.error('Error al guardar la instalación:', error)
+    }
+  }
+
+<<<<<<< HEAD
     const [mostrarMensajeExito, setMostrarMensajeExito] = useState({ mostrar: false, mensaje: '', variant: '' })
 
     useEffect(() => {
@@ -217,6 +278,125 @@ InstalacionModal.propTypes = {
     cerrarModal: PropTypes.func,
     instalacion: PropTypes.object,
     actualizarInstalacion: PropTypes.func.isRequired,
+=======
+  const limpiarDatos = () => {
+    setNombre('')
+    setLocalidad('')
+    setCostoInstalacion('')
+    setCapacidadInstalacion('')
+    setDescripcionInstalacion('')
+    setImagenPrincipal('')
+  }
+
+  return (
+    <Modal
+      open={openModal}
+      onClose={cerrarModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography
+          variant="h6"
+          align="center"
+          gutterBottom
+          style={{ color: instalacion ? 'black' : 'black' }}
+        >
+          {instalacion ? 'Editar Instalación' : 'Crear Instalación'}
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <div
+            style={{ display: 'flex', flexDirection: 'column', color: 'black' }}
+          >
+            <TextField
+              id="nombre"
+              name="nombre"
+              label="Nombre"
+              variant="standard"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              style={{ marginBottom: '1rem' }}
+            />
+            <TextField
+              id="localidad"
+              name="localidad"
+              label="Localidad"
+              variant="standard"
+              value={localidad}
+              onChange={(e) => setLocalidad(e.target.value)}
+              style={{ marginBottom: '1rem' }}
+            />
+            <TextField
+              id="costo"
+              name="costo"
+              label="Costo"
+              variant="standard"
+              value={costoInstalacion}
+              onChange={(e) => setCostoInstalacion(e.target.value)}
+              style={{ marginBottom: '1rem' }}
+            />
+            <TextField
+              id="capacidad"
+              name="capacidad"
+              label="Capacidad"
+              variant="standard"
+              value={capacidadInstalacion}
+              onChange={(e) => setCapacidadInstalacion(e.target.value)}
+              style={{ marginBottom: '1rem' }}
+            />
+            <TextField
+              id="descripcion"
+              name="descripcion"
+              label="Descripción"
+              variant="standard"
+              value={descripcionInstalacion}
+              onChange={(e) => setDescripcionInstalacion(e.target.value)}
+              style={{ marginBottom: '1rem' }}
+            />
+            {image && (
+              <TextField
+                id="imagen"
+                name="imagen"
+                label="Imagen"
+                type="file"
+                onChange={uploadToServer}
+                variant="standard"
+                value={imagenPrincipal}
+                //onChange={(e) => setImagenPrincipal(e.target.value)}
+                style={{ marginBottom: '1rem' }}
+              />
+            )}
+            <TextField
+              id="descripcion2"
+              name="descripcion2"
+              label="Descripción2"
+              variant="standard"
+              value={image}
+              disabled={true}
+              style={{ marginBottom: '1rem' }}
+            />
+            {/* Agrega más campos si es necesario para la instalación */}
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button variant="text" onClick={cerrarModal}>
+                Cancelar
+              </Button>
+              <Button type="submit" variant="text">
+                {instalacion ? 'Guardar Cambios' : 'Crear'}
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Box>
+    </Modal>
+  )
+}
+
+InstalacionModal.propTypes = {
+  openModal: PropTypes.bool,
+  cerrarModal: PropTypes.func,
+  instalacion: PropTypes.object,
+  actualizarInstalacion: PropTypes.func.isRequired,
+>>>>>>> 9841c9d ( se agrega funcionalidad al formulario de nuevas instalaciones cargar imagenes al momento de generar un nuevo registro)
 }
 
 export default InstalacionModal
