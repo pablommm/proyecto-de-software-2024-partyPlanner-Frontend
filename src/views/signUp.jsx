88916b8680ac495e7c06622b01925a562f0523d0
fedemv <fedemv93@gmail.com>
2024-06-20 11:16:18 -0300

    agrego icono para ver contraseña en signUp

--- src/views/signUp.jsx
+++ src/views/signUp.jsx
@@ -1,173 +1,201 @@
-
-
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { Checkbox, Container, FormControlLabel, Grid, Alert } from '@mui/material'
-import { Link as RouterLink } from "react-router-dom"
+import {
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Alert,
+} from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
 import { useState } from 'react'
 import { UsuarioRegistro } from 'src/Dominio/Usuario'
 import usuarioService from 'src/Services/usuario.service'
 import { useNavigate } from 'react-router-dom'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { IconButton, InputAdornment } from '@mui/material'
 
 const SignUpView = () => {
-    const navigate = useNavigate()
-    const [nombre, setNombre] = useState('')
-    const [apellido, setApellido] = useState('')
-    const [username, setUsername] = useState('')
-    const [pwd, setPwd] = useState('')
-    const [rol, setRol] = useState(false)
+  const navigate = useNavigate()
+  const [nombre, setNombre] = useState('')
+  const [apellido, setApellido] = useState('')
+  const [username, setUsername] = useState('')
+  const [pwd, setPwd] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [rol, setRol] = useState(false)
+  const [error, setError] = useState(false)
 
-    const [error, setError] = useState(false)
+  const crear = async () => {
+    const nuevoUsuario = new UsuarioRegistro()
+    nuevoUsuario.nombre = nombre
+    nuevoUsuario.apellido = apellido
+    nuevoUsuario.usuario = username
+    nuevoUsuario.pwd = pwd
+    nuevoUsuario.rol = rol ? 'PROPIETARIO' : 'CONSUMIDOR'
 
-    const crear = async () => {
-        const nuevoUsuario = new UsuarioRegistro()
-        nuevoUsuario.nombre = nombre
-        nuevoUsuario.apellido = apellido
-        nuevoUsuario.usuario = username
-        nuevoUsuario.pwd = pwd
-        nuevoUsuario.rol = rol ? 'PROPIETARIO' : 'CONSUMIDOR'
-
-
-
-        console.log("Nuevo usuario:", nuevoUsuario)
-        try {
-
-            const respuestaCrearUsuario = await usuarioService.crearUsuario(nuevoUsuario)
-            console.log("Respuesta de creación de evento:", respuestaCrearUsuario)
-            console.log("usuario creado exitosamente.")
-            navigate('/login')
-        }
-        catch {
-            setError('Este usuario ya existe. Intentelo nuevamente.')
-        }
-
+    console.log('Nuevo usuario:', nuevoUsuario)
+    try {
+      const respuestaCrearUsuario =
+        await usuarioService.crearUsuario(nuevoUsuario)
+      console.log('Respuesta de creación de evento:', respuestaCrearUsuario)
+      console.log('usuario creado exitosamente.')
+      navigate('/login')
+    } catch {
+      setError('Este usuario ya existe. Intentelo nuevamente.')
     }
+  }
+  
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('se crea el usuario')
+    crear()
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log("se crea el usuario")
-        crear()
-    }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword)
+  }
 
-    return (
-        <Container maxWidth="xs" className='main'>
+  return (
+    <Container maxWidth="xs" className="main">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          mt: 3,
+        }}
+      >
+        <img
+          src="/logoIII.png"
+          alt="Logo"
+          style={{ width: '150px', marginBottom: '1rem' }}
+        />
 
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    mt: 3,
+        <Typography
+          component="h1"
+          variant="h5"
+          style={{ marginBottom: '1rem' }}
+        >
+          Registrate
+        </Typography>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="Nombre"
+                required
+                fullWidth
+                id="nombre"
+                label="Nombre"
+                autoFocus
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="Apellido"
+                required
+                fullWidth
+                id="Apellido"
+                label="Apellido"
+                autoFocus
+                value={apellido}
+                onChange={(e) => setApellido(e.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="given-name"
+                name="UserName"
+                required
+                fullWidth
+                id="userName"
+                label="username"
+                autoFocus
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Contraseña"
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                autoComplete="new-password"
+                value={pwd}
+                onChange={(e) => setPwd(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={togglePasswordVisibility} edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
                 }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                value="start"
+                labelPlacement="start"
+                label="¿Quiere publicar su Instalación?"
+                control={
+                  <Checkbox
+                    checked={rol}
+                    onChange={(e) => setRol(e.target.checked)}
+                    color="primary"
+                  />
+                }
+                sx={{ '& .MuiFormControlLabel-label': { color: 'black' } }}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 1, mb: 2 }}
+          >
+            Registrarme
+          </Button>
+          <Box sx={{ textAlign: 'center' }}>
+            <RouterLink to="/login" variant="body2">
+              ¿Ya tienes una cuenta? Inicia sesión
+            </RouterLink>
+          </Box>
+          {error && (
+            <Alert
+              severity="error"
+              style={{ position: 'absolute', bottom: '60px' }}
             >
-                <img src="/logoIII.png" alt="Logo" style={{ width: '150px', marginBottom: '1rem' }} />
-
-                <Typography component="h1" variant="h5" style={{ marginBottom: '1rem', color: 'black' }}>
-                    Registrate
-                </Typography>
-                <form onSubmit={handleSubmit}
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: '1rem',
-                    }}
-                >
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="given-name"
-                                name="Nombre"
-                                required
-                                fullWidth
-                                id="nombre"
-                                label="Nombre"
-                                autoFocus
-                                value={nombre}
-                                onChange={(e) => setNombre(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="given-name"
-                                name="Apellido"
-                                required
-                                fullWidth
-                                id="Apellido"
-                                label="Apellido"
-                                autoFocus
-                                value={apellido}
-                                onChange={(e) => setApellido(e.target.value)}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12} >
-                            <TextField
-                                autoComplete="given-name"
-                                name="UserName"
-                                required
-                                fullWidth
-                                id="userName"
-                                label="username"
-                                autoFocus
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                label="Contraseña"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                value={pwd}
-                                onChange={(e) => setPwd(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                value="start"
-                                labelPlacement="start"
-                                label="¿Quiere publicar su Instalación?"
-                                control={
-                                    <Checkbox
-                                        checked={rol}
-                                        onChange={(e) => setRol(e.target.checked)}
-                                        color="primary"
-                                    />
-                                }
-                                sx={{ '& .MuiFormControlLabel-label': { color: 'black' } }}
-                            />
-                        </Grid>
-
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 1, mb: 2 }}
-                    >
-                        Registrarme
-                    </Button>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <RouterLink to="/login" variant="body2" >
-                            ¿Ya tienes una cuenta? Inicia sesión
-                        </RouterLink>
-                    </Box>
-                    {error && <Alert severity="error" style={{ position: 'absolute', bottom: '60px' }}> {error}</Alert>
-                    }
-
-                </form>
-            </Box>
-        </Container>
-
-    )
+              {' '}
+              {error}
+            </Alert>
+          )}
+        </form>
+      </Box>
+    </Container>
+  )
 }
 export default SignUpView
